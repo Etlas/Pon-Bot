5165d6b491e24c9c58ed4c2327c67464d877a5e5
Etlas <musicinsilence13@gmail.com>
2014-01-25 05:28:20 +0000

    new pon

--- pon.py
+++ pon.py
@@ -9,60 +9,57 @@ mrk = "DOGE/BTC"
 hist = 0
 buyOrd = 0
 sellOrd = 0
-ActiveOrder = false
 prevPrice = 0
 prevTrade = "sell"
-bitBal = 0
-dogBal = 0
+bitBal = 0.00002768
+dogBal = 300
 
+def buy(price):
+        doge = float(bitBal) / (float(price) * float(1.002))
+        #c.place_order(c.markets[mkr], "Buy", price, doge)
+        print("Bought " + str(doge) + " amount of Dogecoin for " + str(price))
+        with open("log.txt", "a") as file:
+                file.write("Bought " +str(doge) + " amount of Dogecoin for " +str(price))
+
+def sell(price):
+        #c.place_order(c.markets[mkr], "Sell", price, dogBal)
+        print("Sold " + str(dogBal) + " amount of Dogecoin for " + str(price))
+        with open("log.txt", "a") as file:
+                file.write("Sold " + str(dogBal) + " amount of Dogecoin for " + str(price))
 
 while True:
 	time.sleep(10)
 	tyme = datetime.datetime.now().time()
 	try:
 		c.update_markets()
-		c.updates_orders_by_market(c.markers[mrk])
+		c.update_orders_by_market(c.markets[mrk])
 		c.update_trade_history(c.markets[mrk])
 		c.update_my_open_orders(c.markets[mrk])
 		c.update_balance()
 	except ValueError:
-		print "Value Error; couldn't connect to cryptsy"
+		print("Value Error; couldn't connect to cryptsy")
 		with open("log.txt", "a") as file:
-			file.write(tyme + ": Value Error; couldn't connect to cryptsy")
+			file.write(str(tyme) + ": Value Error; couldn't connect to cryptsy")
 	except KeyError:
-		print "Key Error; couldn't connect to cryptsy"
+		print("Key Error; couldn't connect to cryptsy")
 		with open("log.txt", "a") as file:
-			file.write(tyme + ": Key Error; couldn't connect to cryptsy")
+			file.write(str(tyme) + ": Key Error; couldn't connect to cryptsy")
 	hist = c.markets[mrk].history[0].price
 	buyOrd = c.markets[mrk].buy_orders[0].price
 	sellOrd = c.markets[mrk].sell_orders[0].price
 	b = c.get_bank()
 	bitBal = b.coins["BTC"]
-	dogBal = b.coins["DOGE"]
-	if ((len(c.markets[mrk].my_orders) == 0):
-		print "No orders! Looking to trade!"
-		if (prevTrade == "sell"):
-			if (math.fabs(hist - buyOrd) > 10):
-				buy(hist)
-				prevTrade = "buy"
-				prevPrice = hist
-		if (prevTrade == "buy"):
-			if (math.fabs(hist - prevPrice) > 2):
-				sell(hist)
-				prevTrade = "sell"
-				prevPrice = hist
+	#dogBal = b.coins["DOGE"]
+	print("Latest Price: " + str(hist) + " | Buy: " + str(buyOrd) + " | Sell: " + str(sellOrd))
+	if (prevTrade == "sell"):
+		buy(hist)
+		prevTrade = "buy"
+		prevPrice = hist
+	if (prevTrade == "buy"):
+		if (math.fabs(float(hist) - float(prevPrice)) > 0.00000002):
+			sell(hist)
+			prevTrade = "sell"
+			prevPrice = hist
 
 
-def buy(price):
-	doge = float(bitBal) / (float(price) * float(1.002))
-	c.place_order(c.markets[mkr], "Buy", price, doge)
-	print "Bought " + doge + " amount of Dogecoin for " + price
-	with open("log.txt", "a") as file:
-		file.write("Bought " + doge + " amount of Dogecoin for " + price)
-
-def sell(price):
-	c.place_order(c.markets[mkr], "Sell", price, dogBal)
-	print "Sold " + dogBal " amount of Dogecoin for " + price
-	with open("log.txt", "a") as file:
-		file.write("Sold " + dogBal + " amount of Dogecoin for " + price)
 	
