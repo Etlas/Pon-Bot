7bd38a6c55d42aca130397f7a4cd15bf1c1ab1d6
Etlas <musicinsilence13@gmail.com>
2014-02-03 22:30:45 +0000

    such messy code

--- pon.py
+++ pon.py
@@ -19,6 +19,7 @@ dogBal = 0
 pastTradeD = 0
 pastTradeP = 0
 pastTradeT = "sell"
+connection = False
 
 def email(type,price,doge):
 	sender = "Pon Trader"
@@ -41,36 +42,44 @@ def email(type,price,doge):
 
 def buy(price):
         doge = float(bitBal) / (float(price) * float(1.002))
-        c.place_order(c.markets[mrk], "Buy", price, doge)
-        prevTradeT = "buy"
-        prevTradeP = price
-        prevTradeD = doge
-	global dogBal
-	dogBal = doge
-	global bitBal
-	bitBal = 0
-        print("--Bought " + str(doge) + " amount of Dogecoin for " + str(price))
-        with open("log.txt", "a") as file:
-                file.write("Bought " +str(doge) + " amount of Dogecoin for " +str(price))
+	try:
+	        c.place_order(c.markets[mrk], "Buy", price, doge)
+	        pastTradeT = "buy"
+	        pastTradeP = price
+	        pastTradeD = doge
+		global dogBal
+		dogBal = doge
+		global bitBal
+		bitBal = 0
+	        print("--Bought " + str(doge) + " amount of Dogecoin for " + str(price))
+	        with open("log.txt", "a") as file:
+	                file.write("Bought " +str(doge) + " amount of Dogecoin for " +str(price))
+	except:
+		print("buy failed")
 
 def sell(price):
-        c.place_order(c.markets[mrk], "Sell", price, dogBal)
-        prevTradeT = "sell"
-        prevTradeP = price
-        prevTradeD = dogBal
-        print("-Sold " + str(dogBal) + " amount of Dogecoin for " + str(price))
-	global bitBal
-	bitBal = float(dogBal) * (float(price) * float(1.002))
-	global dogBal
-	dogBal = 0
-        with open("log.txt", "a") as file:
-                file.write("Sold " + str(dogBal) + " amount of Dogecoin for " + str(price))
+	try:
+	        c.place_order(c.markets[mrk], "Sell", price, dogBal)
+	        pastTradeT = "sell"
+	        pastTradeP = price
+	        pastTradeD = dogBal
+	        print("-Sold " + str(dogBal) + " amount of Dogecoin for " + str(price))
+		global bitBal
+		bitBal = float(dogBal) * (float(price) * float(1.002))
+		global dogBal
+		dogBal = 0
+	        with open("log.txt", "a") as file:
+	                file.write("Sold " + str(dogBal) + " amount of Dogecoin for " + str(price))
+	except:
+		print("sell failed")
                 
 try:
 	c.update_balance()
 	c.update_my_open_orders(c.markets[mrk])
 	ords = len(c.markets[mrk].my_orders)
 except KeyError:
+	ords = 0
+except:
 	ords = 0
 b = c.get_bank()
 try:
@@ -87,24 +96,28 @@ else:
 	prevTrade = "buy"
 print("prevTrade = " + str(prevTrade) + " DogBal = " + str(dogBal) + " BitBal = " + str(bitBal))
 while True:
-	time.sleep(10)
+	time.sleep(20)
 	tyme = datetime.datetime.now().time()
 	try:
 		c.update_markets()
 		c.update_orders_by_market(c.markets[mrk])
 		c.update_trade_history(c.markets[mrk])
 		c.update_my_open_orders(c.markets[mrk])
+		connection = True
 	except ValueError:
 		print("Value Error; couldn't connect to cryptsy")
 		with open("log.txt", "a") as file:
 			file.write(str(tyme) + ": Value Error; couldn't connect to cryptsy")
+		connection = False
 	except KeyError:
 		print("Key Error; couldn't connect to cryptsy")
 		with open("log.txt", "a") as file:
 			file.write(str(tyme) + ": Key Error; couldn't connect to cryptsy")
-	hist = c.markets[mrk].history[0].price
-	buyOrd = c.markets[mrk].buy_orders[0].price
-	sellOrd = c.markets[mrk].sell_orders[0].price
+		connection = False
+	if (connection == True):
+		hist = c.markets[mrk].history[0].price
+		buyOrd = c.markets[mrk].buy_orders[0].price
+		sellOrd = c.markets[mrk].sell_orders[0].price
 	if (len(c.markets[mrk].my_orders) == 0):
 		email(pastTradeT, pastTradeP, pastTradeD)
 		print("Latest Price: " + str(hist) + " | Buy: " + str(buyOrd) + " | Sell: " + str(sellOrd))
